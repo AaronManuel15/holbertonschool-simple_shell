84ffa5f84e8bfbfba8b65e6e9ce6421ab3e0d7ad
AaronManuel15 <aaronmanuel15@gmail.com>
2022-07-25 09:56:35 -0700

    Added templates for group work

---
+++ shell.h
@@ -0,0 +1,26 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+extern char **environ;
+
+typedef struct path
+{
+    char *directory;
+    struct path *nextdir;
+} path_t;
+
+char *_which(char *);
+char *_getenv(const char *name);
+int delimcount(char *buffer);
+path_t *add_path_node(path_t **head, const char *str);
+
+#endif
